d395d51f2aaa6828de9adbe57dfa13a0f03709dc
SergeyGrigoryan2107-sus <segreigrigryan2207@mail.ru>
2025-05-13 21:09:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -95,13 +95,97 @@ first_index = 0;
 last_index = 0;
 sum = 0
 for i in range(0, len(numbers)):
-    if numbers[i]> 0:
+    if numbers[i] > 0:
         first_index = i
         break
-for i in range( len(numbers) - 1,-1,-1):
-    if numbers[i]> 0:
+for i in range(len(numbers) - 1, -1, -1):
+    if numbers[i] > 0:
         last_index = i
         break
-for i in range(first_index+1, last_index):
+for i in range(first_index + 1, last_index):
     sum += numbers[i]
 print(sum, end=" ")'''
+'''list = [2,3,4]
+print(list)
+list.append(6)
+print(list)
+list.clear()
+print(list)'''
+"""list = [2, 3, 4]
+print('''выберите действие 1
+выберите действие 2
+выберите действие 3
+4.показать список
+5.количество элементов в списке
+6.удаление с конкретных позиции
+7.удаление конктретного элемента 
+7.добавление конкретного элемента 
+8.разворот в обратном порядке
+9.сортировка списка
+10.выход.''')
+valid_check = True
+while valid_check:
+    check = int(input("выберите действие"))
+    if check < 1 or check > 10:
+        print("неккоректное значение")
+    else:
+        break
+if check == 1:
+    num1 = int(input("введите значение:"))
+    list.sort()
+elif check == 2:
+    list.clear()
+    print("список очищен")
+elif check == 3:
+    print(list)
+elif check == 4:
+    print("длина списка", len(list))
+elif check == 5:
+    index = int(input(''))
+    if (index < 0 or index > len(list)):
+        print("не существующий индекс")
+    else:
+        list.pop(index)
+elif check == 6:
+    try:
+        list.remove(int(input("введите значение для удаления")))
+    except:
+        print("данное значение отсутствует")
+elif check == 7:
+    index = int(input(''))
+    if (index < 0 or index > len(list)):
+        print("не существующий индекс")
+    else:
+        num = int(input("введите значение"))
+        list.insert(index, num)
+elif check == 8:
+    list.reverse()
+    print("список развернут")
+elif check == 9:
+    list.sort()
+    print("писок отсортирован")
+elif check == 10:
+    exit()
+else:
+    print("неккоректное значение"""
+
+
+'''line="hbadlajhvlhvbdaljc sdlvabsd"
+line2=" "
+for i in line:
+    line2=i+line2
+print(line2)'''
+'''ss="7 2 2 3 4 5 6 7"
+ll=list(ss)
+ll.reverse()
+ss2=" "
+for i in ll:
+    ss2+=i
+print(ss2)'''
+'''line=input()
+symbol=input()
+print(line.count(symbol))'''
+'''line=input()
+symbol=input()
+counter=0
+for i in range'''
